0dc1316a169abcd1b5ed7ee1c5f818a274e8f2b6
jorell1 <jorell1@umbc.edu>
2020-11-03 16:50:23 -0500

    About to add the Jaro code

--- RunPyLZJD.py
+++ RunPyLZJD.py
@@ -119,7 +119,7 @@ def plot_hist(data):
     plt.show()
 
 
-def plot_scatter(data):
+def plot_scatter(data, title=""):
     filenames = data.keys()
     x = []
     y = []
@@ -134,7 +134,7 @@ def plot_scatter(data):
     for i, name in enumerate(data):
         plt.annotate(name, (x[i], y[i]))
     plt.plot((0, 1), 'r--')
-
+    plt.title = title
     plt.show()
 
 
@@ -145,6 +145,12 @@ def run_ttest(data1, data2):
 
 
 def run_keyword_test():
+    print("""
+    +++++++++++++++++++++++++++++++
+    +++ Performing Keyword Test +++
+    +++++++++++++++++++++++++++++++
+    """)
+
     for f in listdir(SRC):
         if isfile(join(SRC, f)):
             print("Keyword test: Running on {}".format(f))
@@ -152,7 +158,7 @@ def run_keyword_test():
             KWSEQS_DIGESTS[f] = {'src': None, 'r2': None, 'ghidra': None}
 
             # calculate the digest of the source sequence
-            #print("Keyword test: Getting digest...".format(f))
+            # print("Keyword test: Getting digest...".format(f))
             seq_src = get_keyword_sequence(join(SRC, f))
 
             KWSEQS_DIGESTS[f]['src'] = digest(seq_src)
@@ -165,15 +171,16 @@ def run_keyword_test():
             seq_R2 = get_keyword_sequence(join(R2DEC_PATH, R2DEC_NAME.format(f2)))
             KWSEQS_DIGESTS[f]['r2'] = digest(seq_R2)
 
-            print_seq = len(seq_src) > 300 and (len(seq_GDR) < 50 or len(seq_R2) < 50)
+            # print_seq = len(seq_src) > 300 and (len(seq_GDR) < 50 or len(seq_R2) < 50)
 
-            if f in PRINT_SEQ:
-                print("Sequence for ", f)
-                print("SRC:", seq_src)
-                print("GDR:", seq_GDR)
-                print("R2:", seq_R2)
+            # if f in PRINT_SEQ:
+            #     print("Sequence for ", f)
+            #     print("SRC:", seq_src)
+            #     print("GDR:", seq_GDR)
+            #     print("R2:", seq_R2)
             # obtain the similarity from source
-            #print("Keyword test: Comparing...".format(f))
+            # print("Keyword test: Comparing...".format(f))
+
             KWSEQS_SCORES[f] = {'ghidra': get_lzjd_sim(KWSEQS_DIGESTS[f]['src'], KWSEQS_DIGESTS[f]['ghidra']),
                                 'r2': get_lzjd_sim(KWSEQS_DIGESTS[f]['src'], KWSEQS_DIGESTS[f]['r2']),
                                 'x': get_lzjd_sim(KWSEQS_DIGESTS[f]['ghidra'], KWSEQS_DIGESTS[f]['r2'])}
@@ -190,6 +197,20 @@ def run_keyword_test():
         if KWSEQS_SCORES[f]['ghidra'] > KWSEQS_SCORES[f]['r2']:
             gidra_doms += 1
     print("Ghidra Dominated on {} files".format(gidra_doms))
+
+    plot_scatter(KWSEQS_SCORES, "LZJD Keyword Sequence Scores")
+
+    # obtian the scores as input data to the plots
+    bxplt_data_gd = [score['ghidra'] for score in KWSEQS_SCORES.values()]
+    bxplt_data_r2 = [score['r2'] for score in KWSEQS_SCORES.values()]
+
+    # create plots
+    plot_boxplt([bxplt_data_gd, bxplt_data_r2], 'Keyword LZJD: Ghidra vs R2')
+    plot_hist(bxplt_data_gd)
+    plot_hist(bxplt_data_r2)
+
+    print("Performing T-Test on Keyword Test Scores")
+    run_ttest(bxplt_data_gd, bxplt_data_r2)
 
 
 def run_levenshtein_test():
@@ -304,6 +325,8 @@ def run_levenshtein_kw_test():
     print("Performing T-Test on Levenshtein Distnace of sequences")
     run_ttest(bxplt_data_gd, bxplt_data_r2)
 
+    plot_scatter(LEV_SCORES, "Levenshtein Keyword sequence scores")
+
 
 def main(args):
     # grabbing parameters form arg parse
@@ -344,7 +367,7 @@ def main(args):
 
     # This test will take all the files in the folders provided in the arguments and will perform an LZJD test
     # with the source vs the output files
-    run_main_test()
+    # run_main_test()
 
     ####################
     ## Prepare Plots ###
@@ -353,13 +376,13 @@ def main(args):
     # This section of code prepares visualizations on the data for easy analysis
 
     # obtian the scores as input data to the plots
-    bxplt_data_gd = [score['ghidra'] for score in SCORES.values()]
-    bxplt_data_r2 = [score['r2'] for score in SCORES.values()]
+    # bxplt_data_gd = [score['ghidra'] for score in SCORES.values()]
+    # bxplt_data_r2 = [score['r2'] for score in SCORES.values()]
 
     # create plots
-    plot_boxplt([bxplt_data_gd, bxplt_data_r2], 'LZJD: Ghidra vs R2')
-    plot_hist(bxplt_data_gd)
-    plot_hist(bxplt_data_r2)
+    # plot_boxplt([bxplt_data_gd, bxplt_data_r2], 'LZJD: Ghidra vs R2')
+    # plot_hist(bxplt_data_gd)
+    # plot_hist(bxplt_data_r2)
 
     #######################
     ### Pairwise T-test ###
@@ -367,8 +390,8 @@ def main(args):
 
     # After preparing the plots its a great idea to run a T-test to see if there is a difference in the means of
     # the data. This essentially tells us if one of the decompilers outperformed the other.
-    print("Performing T-Test on Main Test Scores")
-    run_ttest(bxplt_data_gd, bxplt_data_r2)
+    # print("Performing T-Test on Main Test Scores")
+    # run_ttest(bxplt_data_gd, bxplt_data_r2)
 
     #######################
     ### Keyword Testing ###
@@ -378,32 +401,17 @@ def main(args):
     # run the LZJD algorithms again to compare keword sequences.
     run_keyword_test()
 
-    ####################
-    ## Prepare Plots ###
-    ####################
-
-
-    # obtian the scores as input data to the plots
-    bxplt_data_gd = [score['ghidra'] for score in KWSEQS_SCORES.values()]
-    bxplt_data_r2 = [score['r2'] for score in KWSEQS_SCORES.values()]
-
-    # create plots
-    plot_boxplt([bxplt_data_gd, bxplt_data_r2], 'Keyword LZJD: Ghidra vs R2')
-    plot_hist(bxplt_data_gd)
-    plot_hist(bxplt_data_r2)
-
-    print("Performing T-Test on Keyword Test Scores")
-    run_ttest(bxplt_data_gd, bxplt_data_r2)
 
     # LEVENSHTEIN on the full files does not make sense....
-    if show_lev is True:
-        run_levenshtein_test()
+    # if show_lev is True:
+    #     run_levenshtein_test()
 
     # This will make the same process of extracting the keyword squences and perform the levenshtein distance
     # analysis on the sequences to see how far are they from each other
     run_levenshtein_kw_test()
 
-    plot_scatter(KWSEQS_SCORES)
+    # This is just so to see if the Jaro algorithms coincides with LZJD or the Levenshtein scores
+    
 
     print("Done.")
 
